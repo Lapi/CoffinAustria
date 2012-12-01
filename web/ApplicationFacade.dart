7c761b66087fab439784322a701fd9262b7f64e7
Lapi <markus.lapatschek@gmail.com>
2012-12-01 16:57:59 +0100

    .

--- web/ApplicationFacade.dart
+++ web/ApplicationFacade.dart
@@ -2,7 +2,16 @@ import 'dart:html';
 import 'dart:core';
 import 'package:puremvc/puremvc.dart';
 
-class ApplicationFacade extends Facade implements IFacade{
-  static const String STARTUP = "startup";
-  static const String LOGIN = "login";
-}
+//class ApplicationFacade extends Facade implements IFacade{
+//  static const String STARTUP = "startup";
+//  static const String LOGIN = "login";
+//  
+////  void ApplicationFacade()
+////  {
+////    
+////  }
+////  static void getInstance(): ApplicationFacade
+////  {
+////    
+////  }
+//}
